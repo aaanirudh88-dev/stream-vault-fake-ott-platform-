7567ee9ea304412157e82507d92e28fb4c4d36a9
aaanirudh88-dev <aaanirudh88@gmail.com>
2026-07-09 14:16:11 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,232 @@
+ window.movies = [
+    {
+    title : "Scam 1992",
+    image : "https://image.tmdb.org/t/p/original/tTYP1npvBU90NthceezScfXGiOl.jpg",
+    genre : ["Web Series","drama"],
+    video : "https://www.youtube.com/embed/Xvukp6FpUVU?si=G9Q7ieSNc4RxBw68" ,
+    description: "The rise and fall of stockbroker Harshad Mehta."
+
+    },
+    
+    {
+    title : "Dhindora",
+    image : "https://image.tmdb.org/t/p/original/2QsmesFzm5WaP70pRmCbVBzvXDA.jpg",
+    genre : ["Web Series","comedy","drama",],
+    video : "https://www.youtube.com/embed/Y_2y4y0kQig?si=GfmgVfhxFDEzKJGg",
+    description: "A hilarious story of dreams, chaos and family."
+
+
+    },
+    
+
+    {
+    title : "Ekaki",
+    image : "https://image.tmdb.org/t/p/original/pPBebtxm1zJUn1HpbjhoWr0QbQ8.jpg",
+    genre : ["Web Series","Action","sci-fi",],
+    video : "https://www.youtube.com/embed/dB6sowSVcZw?si=Yhhd1IXBCsR5HjC9",
+    description: "A mysterious sci-fi thriller filled with suspense."
+
+
+    },
+
+    {
+        title: "Interstellar",
+        image: "https://image.tmdb.org/t/p/original/ln2Gre4IYRhpjuGVybbtaF4CLo5.jpg",
+        genre : ["Hollywood","sci-fi"],
+        video : src="https://www.youtube.com/embed/zSWdZVtXT7E?si=dHWgkhsUlR5M1bPx",
+        description: "Explorers travel through space to save humanity."
+
+
+    },
+    {
+    title: "Tenet",
+    image: "https://image.tmdb.org/t/p/original/t6rKYahkbfnUpmiX8K3aHEmomDE.jpg",
+    genre: ["Hollywood", "sci-fi"],
+    video :"https://image.tmdb.org/t/p/original/gPucRIw0n3MPv7r1FEdMpngNXr7.jpg",
+    description: "A secret mission where time itself becomes a weapon."
+
+    },
+
+    {
+    title:" Project <br> Hail  <br>  Mary",
+    image: "https://image.tmdb.org/t/p/original/yCXKNBE6fuLX8sc5uqXbXw1bAkK.jpg",
+    genre: ["Hollywood","sci-fi"],
+    video : "https://www.youtube.com/embed/m08TxIsFTRI?si=VAIdhKI-vuEq577v" ,
+    description: "A lone astronaut fights to save Earth."
+
+
+    },
+    {
+    title: "Arrival",
+    image: "https://image.tmdb.org/t/p/original/uKPbFF08QkRMvIAsgCh1soeyPhZ.jpg",
+    genre: ["Hollywood","sci-fi"],
+    video : "https://www.youtube.com/embed/tFMo3UJ4B4g?si=sPOln892gNy6m3WX",
+    description: "Humanity's first contact with alien visitors."
+
+    },
+    {
+    title: " Gullak",
+    image: "https://image.tmdb.org/t/p/original/mB0WU65A41148GtNVcXf5DNQl4z.jpg",
+    genre: ["Web Series","family","drama"],
+    video : "https://www.youtube.com/embed/cs4rnylG7Fc?si=kHIqbDeuPw1-6Bjz" ,
+    description: "Heartwarming tales of a middle-class family."
+
+
+    },
+    
+    {
+    title: "Jawan",
+    image: "https://image.tmdb.org/t/p/original/2hCAXYy11REuOC75DGWJc90IDVw.jpg",
+    genre: ["Bollywood","Action"],
+    video : "https://www.youtube.com/embed/MWOlnZSnXJo?si=jdp6u9X6EXuKW1Xw",
+    description: "A vigilante fights corruption and injustice."
+
+
+    },
+
+    {
+    title: "Kalki 2898AD",
+    image: "https://image.tmdb.org/t/p/original/46S7dEnEx4vPAbDZmqVUhwl4het.jpg",
+    genre: ["Bollywood","Action","sci-fi"],
+    video : "https://www.youtube.com/embed/kQDd1AhGIHk?si=BG_mDjXuCf9lhdKf" ,
+    description: "A futuristic battle between destiny and darkness."
+
+
+    },
+    
+    {
+    title: "Kota<br>Factory",
+    image: "https://image.tmdb.org/t/p/original/nSJR2FiYEvF7LnHiupEpKqglTQc.jpg",
+    genre: ["Web Series","family"],
+    video : "https://www.youtube.com/embed/JWbnEt3xuos?si=DDbc4YRdB7mc4pH3",
+    description: "Students struggle through the pressure of IIT preparation."
+
+
+    },
+    
+    {
+    title: "Loki",
+    image: "https://image.tmdb.org/t/p/original/1pPcHpANG5mGtSYT7MA9QeYOKuK.jpg",
+    genre: ["Web Series","sci-fi","Action"],
+    video : "",
+    description: "The God of Mischief explores alternate timelines."
+
+
+    },
+    
+    {
+    title: "Tamasha",
+    image: "https://image.tmdb.org/t/p/original/sTdFABYywKz6rhpsEWD6vMRIJWA.jpg",
+    genre: ["Bollywood",],
+    video : "" ,
+    description: "A journey of self-discovery and true identity."
+
+ 
+    },
+    
+    {
+    title: "Gram<br>Chikitsalay",
+    image: "https://image.tmdb.org/t/p/original/k2RvAGp7b9Yh2py7NC6ZFZ9N5gR.jpg",
+    genre:["Web Series","family","drama"],
+    video : "https://image.tmdb.org/t/p/original/k2RvAGp7b9Yh2py7NC6ZFZ9N5gR.jpg",
+    description: "Life and challenges in a rural healthcare center."
+
+    },
+    
+    {
+    title: "Brahmastra",
+    image: "https://image.tmdb.org/t/p/original/syURdedVIyLKkDg2nrXM5XWvYSC.jpg",
+    genre: ["Bollywood","Action"],
+    video : "https://www.youtube.com/embed/BUjXzrgntcY?si=hLUeO_i1C3F5Z-Y6" ,
+    description: "A young man discovers his connection to ancient powers."
+
+
+    },
+
+    {
+    title: "Jolly LLB 3",
+    image: "https://image.tmdb.org/t/p/original/gC1kJpDPtaogbq7N543si20jC0w.jpg",
+    genre: ["Bollywood","family","drama"],
+    video : "" ,
+    description: "A courtroom comedy packed with drama and wit."
+
+
+    },
+
+    {
+    title: "Panchayat",
+    image: "https://image.tmdb.org/t/p/original/zxSgUI6443lUgYEXz2ZZoXHty4m.jpg",
+    genre: ["Web Series","family","drama"],
+    video : "https://www.youtube.com/embed/mojZJ7oeD_g?si=c_RwXoLJmBBsYNUy",
+    description: "A city graduate adapts to village life."
+
+
+    },
+    
+    {
+    title: "Atrangi Re",
+    image: "https://image.tmdb.org/t/p/original/1haC16yTzNEdV7K9KRh37p7WhdC.jpg",
+    genre: ["Bollywood","drama"],
+    video : "https://www.youtube.com/embed/x3oMm5JGXZM?si=SY4ZwXUXYBvuifPn" ,
+    description: "An unusual love story with unexpected twists."
+
+
+    },
+
+    {
+    title: "Spiderman: No Way Home",
+    image: "https://image.tmdb.org/t/p/original/tyQo080tijexyUHBvWPwQt26bZa.jpg",
+    genre: ["Hollywood","Action"],
+    video : "https://www.youtube.com/embed/JfVOs4VSpmA?si=gBLWFIi_TqG93PIx" ,
+    description: "Spider-Man faces villains from across the multiverse."
+
+
+    },
+
+    {
+    title: "Sky Force",
+    image: "https://image.tmdb.org/t/p/original/nWB5BjwF1fqLQj1MBnZpUljQqUC.jpg",
+    genre: ["Bollywood","Action"],
+    video : "https://www.youtube.com/embed/PKsVB1wPZ78?si=S4kgjBAk7ROaV35Q",
+    description: "A patriotic action drama inspired by heroism."
+
+
+    },
+
+    {
+    title: "Doctor Strange in the Multiverse of Madness",
+    image: "https://image.tmdb.org/t/p/original/lv3TXqhpaIxkclIHbhN2MRMOemQ.jpg",
+    genre: ["Hollywood","Action"],
+    video : "https://www.youtube.com/embed/aWzlQ2N6qqg?si=mpjh-0jA5Ek2OTct",
+    description: "Doctor Strange battles dangers across the multiverse."
+
+    },
+    
+        {
+    title: "Mission Impossible",
+    image: "https://image.tmdb.org/t/p/original/jnR697dbIYn0erHOQdSFNhHhb1i.jpg",
+    genre: ["Hollywood","Action"],
+    video : "https://www.youtube.com/embed/L8Pbjh4EZRk?si=THvb8tgCRzBS-tby", 
+    description: "Ethan Hunt takes on another impossible mission."
+
+
+    },
+        {
+    title: "Moon<br>Knight",
+    image: "https://image.tmdb.org/t/p/original/tuX6pSxB17ciQ4OmxTToivoSAb9.jpg",
+    genre: ["Web Series","Action"],
+    video : "https://www.youtube.com/embed/x7Krla_UxRg?si=eAgGC1CNXqBHOgCa",
+    description: "A troubled man becomes the avatar of an Egyptian god."
+
+    
+
+    },
+
+    {
+    title: "Inception",
+    image: "https://image.tmdb.org/t/p/original/3hrGstJE0oGG6iFElpq6Vp00u16.jpg",
+    genre: ["Hollywood","sci-fi","Action"],
+    video :"https://www.youtube.com/embed/YoHD9XEInc0?si=QmYJg5jIh6eyufzy",
+    description: "A thief enters dreams to steal hidden secrets."
+    }
+    ]
